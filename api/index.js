1c297c26aa3f7ea387e7ad157d6a90201d5aba4d
calvin-c13 <calvinwchau@gmail.com>
2024-12-26 10:43:35 -0500

    allowed for concurrent frontend and backend servers

--- api/index.js
+++ api/index.js
@@ -15,8 +15,8 @@ mongoose.connect(process.env.MONGO)
         console.log(err);
     });
 
-app.listen(5174, () => {
-    console.log('Server is running on port 5174');
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
 
 app.use('/api/reviews', reviewRouter);
