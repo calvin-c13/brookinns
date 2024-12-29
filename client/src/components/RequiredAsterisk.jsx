5a769c8147194e8dc307b69cf14e1f39228a5cdf
calvin-c13 <calvinwchau@gmail.com>
2024-12-29 11:10:25 -0500

    improved readability in RequiredAsterisk.jsx

--- client/src/components/RequiredAsterisk.jsx
+++ client/src/components/RequiredAsterisk.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React from "react";
 
+// Creates a required asterisk with a hoverable tooltip that says "Required"
 const RequiredAsterisk = () => {
-    return (
-        <span className="relative group">
-            <span className="text-red-500">&nbsp;*&nbsp;</span>
-            <span className="absolute left-0 -mb-1 ml-1 bottom-full opacity-0 group-hover:opacity-80 transition-opacity bg-black text-white text-xs rounded py-1 px-2">
-                Required
-            </span>
-        </span>
-    );
+  return (
+    <span className="relative group">
+      <span className="text-red-500">&nbsp;*&nbsp;</span>
+      <span className="absolute left-0 -mb-1 ml-1 bottom-full opacity-0 group-hover:opacity-80 transition-opacity bg-black text-white text-xs rounded py-1 px-2">
+        Required
+      </span>
+    </span>
+  );
 };
 
 export default RequiredAsterisk;
