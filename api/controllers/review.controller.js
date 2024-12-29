4c38bd3e8f671d6768a1fc07603f9613ab22aa58
calvin-c13 <calvinwchau@gmail.com>
2024-12-29 09:51:21 -0500

    improved readability in review.controller.js

--- api/controllers/review.controller.js
+++ api/controllers/review.controller.js
@@ -3,79 +3,102 @@ import { errorHandler } from "../utils/error.js";
 
 // Create a new review
 export const createReview = async (req, res, next) => {
-    const { side, community, hall, name, major, academicStanding, roomNumber, ac, kitchen, overall, buildingQuality, buildingAmenities, location, socialLife, managementAndStaff, wouldRecommend, reviewText } = req.body;
+  const {
+    side,
+    community,
+    hall,
+    name,
+    major,
+    academicStanding,
+    roomNumber,
+    ac,
+    kitchen,
+    overall,
+    buildingQuality,
+    buildingAmenities,
+    location,
+    socialLife,
+    managementAndStaff,
+    wouldRecommend,
+    reviewText,
+  } = req.body;
 
-    const newReview = new Review({
-        side,
-        community,
-        hall,
-        name,
-        major,
-        academicStanding,
-        roomNumber,
-        ac,
-        kitchen,
-        overall,
-        buildingQuality,
-        buildingAmenities,
-        location,
-        socialLife,
-        managementAndStaff,
-        wouldRecommend,
-        reviewText
-    });
+  const newReview = new Review({
+    side,
+    community,
+    hall,
+    name,
+    major,
+    academicStanding,
+    roomNumber,
+    ac,
+    kitchen,
+    overall,
+    buildingQuality,
+    buildingAmenities,
+    location,
+    socialLife,
+    managementAndStaff,
+    wouldRecommend,
+    reviewText,
+  });
 
-    try {
-        const savedReview = await newReview.save();
-        res.status(201).json(savedReview);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+  // Save the review to the database
+  try {
+    const savedReview = await newReview.save();
+    res.status(201).json(savedReview);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 // Get all reviews
 export const getAllReviews = async (req, res, next) => {
-    try {
-        const reviews = await Review.find();
-        res.status(200).json(reviews);
-    } catch (error) {
-        next(errorHandler(500, "Error fetching reviews"));
-    }
+  try {
+    const reviews = await Review.find();
+    res.status(200).json(reviews);
+  } catch (error) {
+    next(errorHandler(500, "Error fetching reviews"));
+  }
 };
 
 // Get reviews by side (East or West)
 export const getReviewsBySide = async (req, res, next) => {
-    const { side } = req.params;
+  const { side } = req.params;
 
-    try {
-        const reviews = await Review.find({ side });
-        res.status(200).json(reviews);
-    } catch (error) {
-        next(errorHandler(500, `Error fetching reviews for ${side} side`));
-    }
+  try {
+    const reviews = await Review.find({ side });
+    res.status(200).json(reviews);
+  } catch (error) {
+    next(errorHandler(500, `Error fetching reviews for ${side} side`));
+  }
 };
 
 // Get reviews by community within a side
 export const getReviewsByCommunity = async (req, res, next) => {
-    const { side, community } = req.params;
+  const { side, community } = req.params;
 
-    try {
-        const reviews = await Review.find({ side, community });
-        res.status(200).json(reviews);
-    } catch (error) {
-        next(errorHandler(500, `Error fetching reviews for ${community} in ${side} side`));
-    }
+  try {
+    const reviews = await Review.find({ side, community });
+    res.status(200).json(reviews);
+  } catch (error) {
+    next(
+      errorHandler(
+        500,
+        `Error fetching reviews for ${community} in ${side} side`
+      )
+    );
+  }
 };
 
 // Get reviews by hall
 export const getReviewsByHall = async (req, res) => {
-    const { side, community, hall } = req.params;
-    try {
-        const reviews = await Review.find({ side, community, hall });
-        res.status(200).json(reviews);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+  const { side, community, hall } = req.params;
+  try {
+    const reviews = await Review.find({ side, community, hall });
+    res.status(200).json(reviews);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
-
