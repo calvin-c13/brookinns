7f4751ce12c3735c05640c627f83124822f4c310
calvin-c13 <calvinwchau@gmail.com>
2025-01-17 09:40:41 -0500

    changed index.js

--- api/index.js
+++ api/index.js
@@ -2,29 +2,40 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import reviewRouter from "./routes/review.route.js";
+import path from 'path';
 dotenv.config();
-
-const app = express();
-app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log("Connected to MongoDB");
+    console.log('Connected to MongoDB!');
   })
   .catch((err) => {
     console.log(err);
   });
 
+  const __dirname = path.resolve();
+
+const app = express();
+
+app.use(express.json());
+
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log('Server is running on port 3000!');
 });
 
-app.use("/api/reviews", reviewRouter);
+app.use('/api/review', reviewRouter);
+
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || "Internal Server Error";
+  const message = err.message || 'Internal Server Error';
   return res.status(statusCode).json({
     success: false,
     statusCode,
