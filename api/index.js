652b516cc6707568b46262d57c6ddb0cb71bb7d0
calvin-c13 <calvinwchau@gmail.com>
2025-01-19 15:16:48 -0500

    added cors for middleware for getting review count per hall, reorganized index.js

--- api/index.js
+++ api/index.js
@@ -1,44 +1,53 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import path from "path";
+import cors from "cors";
 import reviewRouter from "./routes/review.route.js";
-import path from 'path';
+
 dotenv.config();
 
+//Initialization
+const app = express();
+const __dirname = path.resolve();
+const PORT = process.env.PORT || 3000;
+
+//Database connection
 mongoose
   .connect(process.env.MONGO)
-  .then(() => {
-    console.log('Connected to MongoDB!');
+  .then(() => console.log("Connected to MongoDB!"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+//Middleware
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
   })
-  .catch((err) => {
-    console.log(err);
-  });
-
-  const __dirname = path.resolve();
-
-const app = express();
-
+);
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
+//Routes
+app.use("/api/review", reviewRouter);
+
+//Frontend fallback
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
-app.use('/api/review', reviewRouter);
-
-
-app.use(express.static(path.join(__dirname, '/client/dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
-
+//Error handling
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
+  const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
     success: false,
     statusCode,
     message,
   });
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}!`);
 });
